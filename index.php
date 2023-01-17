01da8e7fa50d21a041940685a5afcfb1bd0d5a27
PaoloCrescenzi <crescenzipaolo2@gmail.com>
2023-01-17 19:38:49 +0100

    ultime correzioni

--- index.php
+++ index.php
@@ -32,49 +32,54 @@ $prodotti = [
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
     <main class="container pb-5">
-        <h1 class="my-5">Php Oop 2 | Index</h1>
+    <h1 class="my-5">Php Oop 2 | Index</h1>
 
-        <div class="row">
-            <?php foreach ($prodotti as $prodotto) { ?>
+    <div class="row">
+      <?php foreach ($prodotti as $prodotto) { ?>
 
-                <div class="col">
-
-                    <div class="card">
-                        <div class="card-body">
-                            <h3><?php echo $prodotto->getName() ?></h3>
-                            <h4><?php echo $prodotto->getPrice() ?></h4>
-
-                            <?php if (method_exists($prodotto, "getCalories")) {
-                                echo "<div>Calorie: {$prodotto->getCalories()}</div>";
-                            } ?>
-
-                            <?php if (method_exists($prodotto, "getMaterials")) { ?>
-                                <div>Materiali: <?php echo implode("<br> ", $prodotto->getMaterials()) ?></div>
-                            <?php } ?>
-                        </div>
-                    </div>
-                </div>
-
-            <?php } ?>
-
-            <!-- <?php foreach ($prodotti as $prodotto) : ?>
         <div class="col">
-        <div class="card"></div>
-        </div>
-    <?php endforeach ?> -->
+          <!-- Se volessimo usare la funzoone printCardHTML della classe prodotto,
+            potrei invocarla direttamente
+            <?php echo $prodotto->printCardHTML($prodotto) ?>
+          -->
+
+          <div class="card">
+            <div class="card-body">
+              <h3><?php echo $prodotto->getName() ?></h3>
+              <h4><?php echo $prodotto->getPrice() ?></h4>
+
+              <!-- <?php if (method_exists($prodotto, "getCalories")) { ?>
+                <div>Calorie: <?php echo $prodotto->getCalories() ?></div>
+              <?php } ?> -->
+
+              <?php if (method_exists($prodotto, "getCalories")) {
+                echo "<div>Calorie: {$prodotto->getCalories()}</div>";
+              } ?>
+
+              <?php if (method_exists($prodotto, "getMaterials")) { ?>
+                <div>Materiali: <?php echo implode("<br> ", $prodotto->getMaterials()) ?></div>
+              <?php } ?>
+            </div>
+          </div>
         </div>
 
-    </main>
+      <?php } ?>
+
+      <!-- <?php foreach ($prodotti as $prodotto) : ?>
+        <div class="col">
+          <div class="card"></div>
+        </div>
+      <?php endforeach ?> -->
+    </div>
+
+  </main>
 </body>
-
 </html>
